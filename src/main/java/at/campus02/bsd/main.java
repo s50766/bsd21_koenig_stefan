8c330912c3c719dc1a5f936a3a1d1e8a32e57c4c
Stefan König <52101510@edu.campus02.at>
2022-05-21 23:14:10 +0200

    Modified pom.xml and added logging to main.java

--- src/main/java/at/campus02/bsd/main.java
+++ src/main/java/at/campus02/bsd/main.java
@@ -1,10 +1,18 @@
 package at.campus02.bsd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import static at.campus02.bsd.Calculator.*;
 
 public class main {
+    private static Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
+        logger.info("It is a info logger.");
+        logger.error("It is an error logger.");
+
+
         Double number1 = 4.0;
         Double number2 = 3.0;
 
@@ -13,7 +21,6 @@ public class main {
         System.out.printf("Calculator : Result: " + divide(number1,number2) + "\n");
         System.out.printf("Calculator * Result: " + multiply(number1,number2) + "\n");
         System.out.printf("Calculator Faculty: " + faculty(number1));
-
 
     }
 
